00c400af479c904595e7ca88fa9a270e0c84221b
= <=>
2023-03-08 12:11:43 +0800

    stuck

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,6 @@ extern fn cb_send_char(msg: *const c_char, id: c_int, user: c_void) -> c_int{
     println!("sendchar: {:?}; {}; {:?}", cstr, id, user);
     return 0;
 }
-
 extern fn cb_send_stat(msg: *const c_char, id: c_int, user: c_void) -> c_int{
     let cstr = unsafe { std::ffi::CStr::from_ptr(msg)};
     println!("sendstat: {:?}; {}; {:?}", cstr, id, user);
@@ -25,17 +24,57 @@ extern fn cb_controlled_exit(status: c_int, immediate: bool, exit_on_quit: bool,
     println!("ctrldexit: {}; {}; {}; {}; {:?}", status, immediate, exit_on_quit, id, user);
     return 0;
 }
+extern fn cb_send_data(pvecvaluesall: *const NgVecvaluesall, count: c_int, id: c_int, user: c_void) -> c_int{
+    println!("senddata: {}; {}; {:?}; {:?};", count, id, user, pvecvaluesall);
+    unsafe{(*pvecvaluesall).debug();}
+    return 0;
+}
+extern fn cb_send_init_data(pvecinfoall: *const i8, count: c_int, id: c_int, user: c_void) -> c_int{
+    println!("sendinitdata: {}; {}; {:?};", count, id, user);
+    return 0;
+}
 extern fn cb_bgthread_running(finished: bool, id: c_int, user: c_void) -> c_int{
     println!("bgrunning: {}; {}; {:?}", finished, id, user);
     return 0;
+}
+
+#[derive(Copy, Clone)]
+#[repr(C)]
+struct NgVecvalues {
+    name: *const c_char,
+    creal: c_double,
+    cimag: c_double,
+    is_scale: bool,
+    is_complex: bool
+}
+impl NgVecvalues {
+    fn debug(self) {
+        println!("{:?}\n{:?}\n{:?}\n{:?}\n{:?}\n", unsafe { std::ffi::CStr::from_ptr(self.name)}, self.creal, self.cimag, self.is_scale, self.is_complex);
+    }
+}
+
+#[derive(Copy, Clone)]
+#[repr(C)]
+struct NgVecvaluesall {
+    count: c_int,
+    index: c_int,
+    vecsa: *const NgVecvalues
+}
+impl NgVecvaluesall {
+    fn debug(self) {
+        println!("{:?}\n{:?}\n{:?}", self.count, self.index, self.vecsa);
+        if let Some(vec1) = unsafe{self.vecsa.as_ref()} {
+            vec1.debug();
+        }
+    }
 }
 
 type NgSpiceInit = fn(
     extern fn(*const c_char, c_int, c_void) -> c_int, 
     extern fn(*const c_char, c_int, c_void) -> c_int, 
     extern fn(c_int, bool, bool, c_int, c_void) -> c_int, 
-    *const c_char, 
-    *const c_char, 
+    extern fn(*const NgVecvaluesall, c_int, c_int, c_void) -> c_int, 
+    extern fn(*const i8, c_int, c_int, c_void) -> c_int,  
     extern fn(bool, c_int, c_void) -> c_int, 
     *const c_char, 
 ) -> bool;
@@ -86,6 +125,6 @@ impl Plugin {
 
 fn main() {
     let ngspice = unsafe { Plugin::new("src/ngspice.dll") };
-    (ngspice.vtable.init)(cb_send_char, cb_send_stat, cb_controlled_exit, std::ptr::null(), std::ptr::null(), cb_bgthread_running, std::ptr::null());
+    (ngspice.vtable.init)(cb_send_char, cb_send_stat, cb_controlled_exit, cb_send_data, cb_send_init_data, cb_bgthread_running, std::ptr::null());
     ngspice.command("source dcop.cir");  // in this case, simulation commands are included inside the netlist and simply sourcing it produces an output.
 }
