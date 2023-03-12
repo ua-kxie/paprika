02dbbba03c3ad0a7fb66a4c39d454b3e959737fa
kxie <kxie@honormicro.com>
2023-03-12 18:13:15 +0800

    updated sharedspice.h reference

--- src/sharedspice.h
+++ src/sharedspice.h
@@ -1,5 +1,5 @@
 /* header file for shared ngspice */
-/* Copyright 2013 Holger Vogt */
+/* Copyright 2021 Holger Vogt */
 /* Modified BSD license */
 
 /*
@@ -14,7 +14,7 @@ defined in the caller are sent to ngspice.dll.
 
 Callback funtion typedefs
 SendChar       typedef of callback function for reading printf, fprintf, fputs
-SendStat       typedef of callback function for reading status string and precent value
+SendStat       typedef of callback function for reading status string and percent value
 ControlledExit typedef of callback function for tranferring a signal upon
                ngspice controlled_exit to caller. May be used by caller
                to detach ngspice.dll.
@@ -43,6 +43,23 @@ receives the name of a vector (may be in the form 'vectorname' or
 <plotname>.vectorname) and returns a pointer to a vector_info struct.
 The caller may then directly assess the vector data (but probably should
 not modify them).
+
+***************** If XSPICE is enabled *************************************
+**
+ngCM_Input_Path(const char*)
+sets the input path for files loaded by code models.
+If NULL is sent, return the current Infile_Path.
+
+**
+ngGet_Evt_NodeInfo(char*)
+receives the name of a event node vector (may be in the form 'vectorname' or
+<plotname>.vectorname) and returns a pointer to a evt_node_info struct.
+The caller may then directly assess the vector data.
+
+**
+char** ngSpice_AllEvtNodes(void);
+returns to the caller a pointer to an array of all event node names.
+****************************************************************************
 
 **
 ngSpice_Circ(char**)
@@ -74,7 +91,18 @@ Memory allocated in the calling program has to be freed only there.
 ngspice.dll should never call exit() directly, but handle either the 'quit'
 request to the caller or an request for exiting upon error,
 done by callback function ngexit().
+
+All boolean signals (NG_BOOL) are of type _Bool, if ngspice is compiled. They
+are of type bool if sharedspice.h is used externally.
 */
+
+#ifndef NGSPICE_PACKAGE_VERSION
+#define NGSPICE_PACKAGE_VERSION "39"
+#endif
+/* we have NG_BOOL instead of BOOL */
+#ifndef HAS_NG_BOOL
+#define HAS_NG_BOOL 1
+#endif
 
 #ifndef NGSPICE_DLL_H
 #define NGSPICE_DLL_H
@@ -112,6 +140,16 @@ struct ngcomplex {
 typedef struct ngcomplex ngcomplex_t;
 #endif
 
+/* NG_BOOL is the boolean variable at the ngspice interface.
+   When ompiling ngspice shared module, typedef to _BOOL, which is boolean in C,
+   when used externally, keep it to be of type bool,
+   as has been available in the past. */
+#ifndef SHARED_MODULE
+typedef bool NG_BOOL;
+#else
+typedef _Bool NG_BOOL;
+#endif
+
 /* vector info obtained from any vector in ngspice.dll.
    Allows direct access to the ngspice internal vector structure,
    as defined in include/ngspice/devc.h . */
@@ -128,8 +166,8 @@ typedef struct vecvalues {
     char* name;        /* name of a specific vector */
     double creal;      /* actual data value */
     double cimag;      /* actual data value */
-    bool is_scale;     /* if 'name' is the scale vector */
-    bool is_complex;   /* if the data are complex numbers */
+    NG_BOOL is_scale;     /* if 'name' is the scale vector */
+    NG_BOOL is_complex;   /* if the data are complex numbers */
 } vecvalues, *pvecvalues;
 
 typedef struct vecvaluesall {
@@ -143,7 +181,7 @@ typedef struct vecinfo
 {
     int number;     /* number of vector, as postion in the linked list of vectors, starts with 0 */
     char *vecname;  /* name of the actual vector */
-    bool is_real;   /* TRUE if the actual vector has real data */
+    NG_BOOL is_real;   /* TRUE if the actual vector has real data */
     void *pdvec;    /* a void pointer to struct dvec *d, the actual vector */
     void *pdvecscale; /* a void pointer to struct dvec *ds, the scale vector */
 } vecinfo, *pvecinfo;
@@ -162,6 +200,24 @@ typedef struct vecinfoall
     pvecinfo *vecs;
 
 } vecinfoall, *pvecinfoall;
+
+/* to be used by ngGet_Evt_NodeInfo, returns all data of a specific node after simulation */
+#ifdef XSPICE
+/* a single data point */
+typedef struct evt_data
+{
+    int           dcop;        /* t.b.d. */
+    double        step;        /* simulation time */
+    char          *node_value; /* one of 0s, 1s, Us, 0r, 1r, Ur, 0z, 1z, Uz, 0u, 1u, Uu */
+} evt_data, *pevt_data;
+
+/* a list of all data points of the node selected by the char* argument to ngGet_Evt_NodeInfo */
+typedef struct evt_shared_data
+{
+    pevt_data *evt_dect; /* array of data */
+    int num_steps;       /* length of the array */
+} evt_shared_data, *pevt_shared_data;
+#endif
 
 
 /* callback functions
@@ -182,11 +238,11 @@ typedef int (SendStat)(char*, int, void*);
    void* return pointer received from caller
 */
 /* asking for controlled exit */
-typedef int (ControlledExit)(int, bool, bool, int, void*);
+typedef int (ControlledExit)(int, NG_BOOL, NG_BOOL, int, void*);
 /*
    int   exit status
-   bool  if true: immediate unloading dll, if false: just set flag, unload is done when function has returned
-   bool  if true: exit upon 'quit', if false: exit due to ngspice.dll error
+   NG_BOOL  if true: immediate unloading dll, if false: just set flag, unload is done when function has returned
+   NG_BOOL  if true: exit upon 'quit', if false: exit due to ngspice.dll error
    int   identification number of calling ngspice shared lib
    void* return pointer received from caller
 */
@@ -208,9 +264,9 @@ typedef int (SendInitData)(pvecinfoall, int, void*);
 */
 
 /* indicate if background thread is running */
-typedef int (BGThreadRunning)(bool, int, void*);
+typedef int (BGThreadRunning)(NG_BOOL, int, void*);
 /*
-   bool        true if background thread is running
+   NG_BOOL        true if background thread is running
    int         identification number of calling ngspice shared lib
    void*       return pointer received from caller
 */
@@ -251,6 +307,38 @@ typedef int (GetSyncData)(double, double*, double, int, int, int, void*);
    void*       return pointer received from caller
 */
 
+#ifdef XSPICE
+/* callback functions
+addresses received from caller with ngSpice_Init_Evt() function
+*/
+
+/* Upon time step finished, called per node */
+typedef int (SendEvtData)(int, double, double, char *, void *, int, int, int, void*);
+/*
+   int         node index
+   double      step, actual simulation time
+   double      dvalue, a real value for specified structure component for plotting purposes
+   char        *svalue, a string value for specified structure component for printing
+   void        *pvalue, a binary data structure
+   int         plen, size of the *pvalue structure
+   int         mode, the mode (op, dc, tran) we are in
+   int         ident, identification number of calling ngspice shared lib
+   void*       return pointer received from caller
+*/
+
+/* Upon initialization, called once per event node
+   To build up a dictionary of nodes */
+typedef int (SendInitEvtData)(int, int, char*, char*, int, void*);
+/*
+   int         node index
+   int         maximum node index, number of nodes
+   char*       node name
+   char*       udn-name, node type
+   int         identification number of calling ngspice shared lib
+   void*       return pointer received from caller
+*/
+#endif
+
 /* ngspice initialization,
 printfcn: pointer to callback function for reading printf, fprintf
 statfcn: pointer to callback function for the status string and percent value
@@ -282,16 +370,39 @@ Commands are executed immediately */
 IMPEXP
 int  ngSpice_Command(char* command);
 
-
 /* get info about a vector */
 IMPEXP
 pvector_info ngGet_Vec_Info(char* vecname);
+
+#ifdef XSPICE
+/* Set the input path for files loaded by code models.
+   If NULL is sent, return the current Infile_Path. */
+IMPEXP
+char* ngCM_Input_Path(const char* path);
+
+/* get info about the event node vector */
+IMPEXP
+pevt_shared_data ngGet_Evt_NodeInfo(char* nodename);
+
+/* get a list of all event nodes */
+IMPEXP
+char** ngSpice_AllEvtNodes(void);
+
+/* initialization of XSPICE callback functions 
+sevtdata: data for a specific event node at time 'step'
+sinitevtdata: single line entry of event node dictionary (list)
+userData: pointer to user-defined data, will not be modified, but
+handed over back to caller during Callback, e.g. address of calling object */
+IMPEXP
+int  ngSpice_Init_Evt(SendEvtData* sevtdata, SendInitEvtData* sinitevtdata, void* userData);
+#endif
 
 
 /* send a circuit to ngspice.dll
    The circuit description is a dynamic array
    of char*. Each char* corresponds to a single circuit
-   line. The last entry of the array has to be a NULL */
+   line. The last-but-one entry of the array has to be a .end card,
+   followed by the last entry NULL. */
 IMPEXP
 int ngSpice_Circ(char** circarray);
 
@@ -314,11 +425,11 @@ char** ngSpice_AllVecs(char* plotname);
 
 /* returns TRUE if ngspice is running in a second (background) thread */
 IMPEXP
-bool ngSpice_running(void);
+NG_BOOL ngSpice_running(void);
 
 /* set a breakpoint in ngspice */
 IMPEXP
-bool ngSpice_SetBkpt(double time);
+NG_BOOL ngSpice_SetBkpt(double time);
 
 
 #ifdef __cplusplus
